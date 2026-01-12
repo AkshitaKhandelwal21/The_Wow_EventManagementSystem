5242954af3c71465c6dea05d9d00d5c264337797
AkshitaKhandelwal21 <khandelwalakshita041@gmail.com>
2026-01-12 11:38:38 +0530

    new event, view events, view event

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new_event', views.CreateEventView.as_view(), name='signup'),
+    path('new_event/', views.CreateEventView.as_view(), name='new_event'),
+    path('org_dash/', views.OrganizerDashboardView.as_view(), name='org-dashboard'),
+    path('my_events/', views.MyEventsView.as_view(), name='my_events'),
+    path('events/<int:id>/', views.ViewEvent.as_view(), name='view_event'),
 ]
