8ab73b13286ce3d6a6cff58403df6eabc840e3cc
AkshitaKhandelwal21 <khandelwalakshita041@gmail.com>
2026-01-12 16:28:20 +0530

    User dashboard, event crud

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -27,3 +27,31 @@ class CreateEventForm(forms.ModelForm):
                 'class': 'form-control'
             }),
         }
+
+
+class EditEventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ['title', 'description', 'category', 'venue', 'date', 'time', 'image']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'What is the event?'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Tell us more'
+            }),
+            'venue': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Where is the event'
+            }),
+            'date': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control'
+            }),
+            'time': forms.TimeInput(attrs={
+                'type': 'time',
+                'class': 'form-control'
+            }),
+        }
--- dashboards/urls.py
+++ dashboards/urls.py
@@ -4,6 +4,11 @@ from . import views
 urlpatterns = [
     path('new_event/', views.CreateEventView.as_view(), name='new_event'),
     path('org_dash/', views.OrganizerDashboardView.as_view(), name='org-dashboard'),
+    path('user_dash/', views.UserDashboardView.as_view(), name='user-dashboard'),
+    path('admin_dash/', views.AdmindashboardView.as_view(), name='admin-dashboard'),
     path('my_events/', views.MyEventsView.as_view(), name='my_events'),
-    path('events/<int:id>/', views.ViewEvent.as_view(), name='view_event'),
+    path('<int:id>/', views.ViewEvent.as_view(), name='view_event'),
+    path('edit_event/<int:pk>/', views.EditEventView.as_view(), name='edit_event'),
+    path('delete_event/<int:pk>/', views.DeleteEventView.as_view(), name='delete_event'),
+    path('all_events/', views.AllEventsView.as_view(), name='all_events'),
 ]
