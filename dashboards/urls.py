8616dfebc907640e3a7beb3773b1bd01e28f78ff
AkshitaKhandelwal21 <khandelwalakshita041@gmail.com>
2026-01-12 18:29:33 +0530

    event registration

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('user_dash/', views.UserDashboardView.as_view(), name='user-dashboard'),
     path('admin_dash/', views.AdmindashboardView.as_view(), name='admin-dashboard'),
     path('my_events/', views.MyEventsView.as_view(), name='my_events'),
-    path('<int:id>/', views.ViewEvent.as_view(), name='view_event'),
+    path('<int:pk>/', views.ViewEvent.as_view(), name='view_event'),
     path('edit_event/<int:pk>/', views.EditEventView.as_view(), name='edit_event'),
     path('delete_event/<int:pk>/', views.DeleteEventView.as_view(), name='delete_event'),
     path('all_events/', views.AllEventsView.as_view(), name='all_events'),
-]
+    path('<int:pk>/register/', views.UserRegisterView.as_view(), name='user-register'),
+]   
