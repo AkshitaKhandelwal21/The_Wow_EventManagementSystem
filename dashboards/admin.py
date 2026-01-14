a1a03123c50d8d6c5871053a40c878d9e5688d71
AkshitaKhandelwal21 <khandelwalakshita041@gmail.com>
2026-01-14 11:04:53 +0530

    Admin panel

--- dashboards/admin.py
+++ dashboards/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 
 from dashboards.models import Event, EventRegistration
 
+
+class EventAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Event
+
+    list_display = [
+        'title', 'description', 'user', 'category', 'image'
+    ]
+    fieldsets = [
+        ("Event Info", {'fields': [
+            'title', 'description', 'category', 'venue', 'image'
+        ]}),
+        ("Events Metadata", {'fields': [
+            'date', 'time', 'address', 'duration', 'seats'
+        ]}),
+        ("Creation Info", {'fields': [
+            'user', 'created_at', 'updated_at'
+        ]})
+    ]
+
 # Register your models here.
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(EventRegistration)
